fdf51650f3d34e81048a7642f0a2a751ce0cf945
pugal26 <pugalcud@gmail.com>
2024-05-11 11:06:41 +0530

    access updated

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import { BookingPage } from './Components/Pages/BookingPage'
 
 axios.defaults.baseURL = 'https://airbnb-api-sjve.onrender.com/';
 axios.defaults.withCredentials = true;
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
 
 
 
